8ac47d7d3c0af67de9ffeb38d05d774a75b92a14
d2a4u <d2a4u@users.noreply.github.com>
2021-06-08 21:33:01 +0100

    Feature/sts (#246)
    * AWS STS implementation 
    
    * Move all errors under one place
    
    All errors should extend from root type of SqsError. However, they are spread between sqs4s.api and sqs4s.auth and errors from sqs4s.auth namespace don't extends SqsError. This commit combine all errors under 1 namespace and introduce backoff retry for auth refresh token mechanism to handle expired token and other retriable errors.
    
    * Fix STS Web Identity Token for EKS (#270)
    
    * Reduce the refresh TTL to 3600 seconds (1 hour)
    
    This makes it compatible with STS Web Identity Token which, by default, does not allow 6 hour tokens.
    
    * Remove ProviderId, must be excluded for non-OAuth2 tokens
    
    Co-authored-by: Christian Palmhøj Nielsen <cpn@famly.co>
    
    #minor

--- native/src/test/scala/sqs4s/api/lo/SendMessageBatchSpec.scala
+++ native/src/test/scala/sqs4s/api/lo/SendMessageBatchSpec.scala
@@ -8,7 +8,7 @@ import org.typelevel.log4cats.slf4j.Slf4jLogger
 import sqs4s.IOSpec
 import sqs4s.auth.Credentials
 import sqs4s.api.SqsConfig
-import sqs4s.api.errors.UnexpectedResponseError
+import sqs4s.errors.UnexpectedResponseError
 import sqs4s.serialization.instances._
 
 import scala.concurrent.duration._
